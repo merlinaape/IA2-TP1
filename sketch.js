641a3732cae9a57c63899c1876a05cde93ffa942
leonflax <leonflaxb@gmail.com>
2024-04-22 20:44:53 -0300

    Primera prueba textura

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let textura;
+
+function preload() {
+  textura = loadImage('img/canvas_texture.jpg');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(0);
+  background(200);
+  noStroke();
+
+  push();
+
+  translate(width / 2, height / 2)
+  fill(200, 100, 0);
+
+  rotate(radians(90));
+  arc(0, 0, 180, 180, 0, PI);
+  fill(100, 200, 0);
+  arc(0, 0, 140, 140, 0, PI);
+  fill(100, 0, 100);
+  arc(0, 0, 100, 100, 0, PI);
+
+  pop();
+
+  push();
+  blendMode(MULTIPLY);
+  tint(255, 100);
+  image(textura, 0, 0);
+  pop();
 }
