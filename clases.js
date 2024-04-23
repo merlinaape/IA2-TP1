f928f8818e59b3d29a6a6034e483d5a42829a266
leonflax <leonflaxb@gmail.com>
2024-04-23 20:00:33 -0300

    Array

--- clases.js
+++ clases.js
@@ -1,6 +1,8 @@
 class Figura {
-    constructor(y) {
+    constructor(y, rad, rot) {
         this.y = y;
+        this.rad = rad;
+        this.rot = rot;
     }
 
     show() {
@@ -9,14 +11,19 @@ class Figura {
         translate(width / 2, height / 2)
         fill(200, 100, 0);
 
-        rotate(radians(90));
-        arc(this.y, 0, 180, 180, 0, PI);
+        if(this.rot == 0 ){
+            rotate(radians(90));
+        }else{
+            rotate(radians(270));
+        }
+
+        arc(this.y, 0, this.rad, this.rad, 0, PI);
         fill(100, 200, 0);
-        arc(this.y, 0, 140, 140, 0, PI);
+        arc(this.y, 0, this.rad / 1.2, this.rad / 1.2, 0, PI);
         fill(100, 0, 100);
-        arc(this.y, 0, 100, 100, 0, PI);
+        arc(this.y, 0, this.rad / 1.7, this.rad / 1.7, 0, PI);
 
         pop();
-
+        console.log(this.rot);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,21 @@
 let textura;
-let figura;
+let figuras = [];
+let indiceFiguras = 0;
+let maxFiguras = 20;
 let cantFiguras = 0;
+let bandera = false;
 
 function setup() {
   createCanvas(windowHeight / 1.5, windowHeight - 50);
-  figura = new Figura(random(50, height - 50));
 }
 
 function draw() {
   background(200);
   noStroke();
 
-  figura.show();
+  for (let i = 0; i < figuras.length; i++) {
+    figuras[i].show();
+  }
 
   push();
   blendMode(MULTIPLY);
@@ -21,5 +25,16 @@ function draw() {
 }
 
 function mouseClicked() {
-  figura = new Figura(random(50, height - 50));
+  if (!bandera) {
+    bandera = true;
+  }
+
+  if (indiceFiguras < maxFiguras) {
+    let posY = random((-height / 2) + 100, (height / 2) - 150);
+    let radio = random(20, 200);
+    let rotacion = Math.round(random());;
+    figuras[indiceFiguras] = new Figura(posY, radio, rotacion);
+    indiceFiguras++;
+  }
+
 }
