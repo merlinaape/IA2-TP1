6b2993d17acf35373c3aff1bf0b8770ebd10a3b6
merlinaape <168295456+merlinaape@users.noreply.github.com>
2024-06-30 17:09:27 -0300

    Add files via upload

--- paleta.js
+++ paleta.js
@@ -1,12 +1,11 @@
 class Paleta {
-    constructor(imagen_) {
-      this.imagen = imagen_; // Imagen de la paleta de colores
-    }
-  
-    darColor() {
-      let x = int(random(this.imagen.width)); // Coordenada X aleatoria en la imagen
-      let y = int(random(this.imagen.height)); // Coordenada Y aleatoria en la imagen
-      return this.imagen.get(x, y); // Retorna el color en la posición aleatoria
-    }
+  constructor(imagen_) {
+    this.imagen = imagen_; // Imagen de la paleta de colores
   }
-  
+
+  darColor() {
+    let x = int(random(this.imagen.width)); // Coordenada X aleatoria en la imagen
+    let y = int(random(this.imagen.height)); // Coordenada Y aleatoria en la imagen
+    return this.imagen.get(x, y); // Retorna el color en la posición aleatoria
+  }
+}
